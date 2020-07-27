66566d68f8c982c5a33c931178b44c3175c828bf
Collin Jilbert <cjilbert504@gmail.com>
2020-07-27 10:42:22 -0500

    Done.

---
+++ src/containers/GifListContainer.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import GifList from '../components/GifList';
+import GifSearch from '../components/GifSearch';
+import GifList from '../components/GifList';
+import GifSearch from '../components/GifSearch';
+
+class GifListContainer extends React.Component {
+    state = {
+        gifs = []
+    }
+
+    fetchGifs = (searchTerm) => {
+        fetch(`https://api.giphy.com/v1/gifs/search?q=${searchTerm}&api_key=ZHgrVdoa9jvi6ntR1LDG22GKh7ovgY0l&rating=g`)
+        .then(res => res.json())
+        .then(data => {
+            this.setState({
+                gifs: data.map(gif => ({ url: gif.images.original.url }))
+            })
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <GifList gifs={this.state.gifs} />
+                <GifSearch fetchGifs={this.fetchGifs} />
+            </div>
+        )
+    }
+
+}
+
+export default GifListContainer;
